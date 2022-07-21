2cd6c3ee83772ba9f82e8696058906612aab91da
Lindsey Brusky <lindsey@potski.com>
2022-07-20 20:04:27 -0500

    Added Family Models

---
+++ Data/Family.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HomeBudget.Data
+{
+    public class Family
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FamilyId { get; set; }
+
+        [StringLength(100)]
+        public string? FamilyName { get; set; }
+
+        [StringLength(450)]
+        public string? Admin { get; set; }
+    }
+}
---
+++ Data/FamilyUser.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeBudget.Data
+{
+    public class FamilyUser
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FamilyUserId { get; set; }
+
+        public int FamilyId { get; set; }
+
+        [StringLength(450)]
+        public string UserId { get; set; }
+    }
+}
