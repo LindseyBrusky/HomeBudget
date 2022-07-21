37b35818c60f7471633118a580c99d32b1e8c0c7
Lindsey Brusky <lindsey@potski.com>
2022-07-20 22:31:13 -0500

    Added Families page

---
+++ Data/FamilyService.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+
+namespace HomeBudget.Data
+{
+    public class FamilyService
+    {
+        private readonly string _connectionString;
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public FamilyService(IConfiguration configuration, ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public Task<List<FamilyUser>> GetFamiliesAsync()
+        {
+            var userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return Task.FromResult(_context.FamilyUsers
+                .Where(fu => fu.UserId == userId)
+                .ToList());
+        }
+            
+    }
+}
